2306e8d0f73deaced44216ac8b677904e7f5928b
James Montemagno <james.montemagno@gmail.com>
2016-08-11 10:46:11 -0700

    Refactor Code!
    Fix up android build as well :)

--- ExpenseIt/completed-code/ExpenseIt/ExpenseIt/Expense.cs
+++ ExpenseIt/completed-code/ExpenseIt/ExpenseIt/Expense.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Plugin.Media.Abstractions;
 
+using static System.DateTime;
+using static System.String;
+
 namespace InvoiceIt
 {
     public class Invoice
     {
-        public double Total { get; set; }
-        public DateTime TimeStamp { get; set; }
-        public string Photo { get; set; }
+        public double Total { get; set; } = 15.0;
+        public DateTime TimeStamp { get; set; } = UtcNow;
+        public string Photo { get; set; } = Empty;
     }
 }
